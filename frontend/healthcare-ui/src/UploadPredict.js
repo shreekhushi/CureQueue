74b3ef726ef6179e05e987fe1eb601d59948a25b
shreekhushi <khushi.shree04@gmail.com>
2025-12-05 21:06:21 +0530

    final code

--- frontend/healthcare-ui/src/UploadPredict.js
+++ frontend/healthcare-ui/src/UploadPredict.js
@@ -10,7 +10,7 @@ export default function UploadPredict() {
     const formData = new FormData();
     formData.append("file", file);
 
-    const res = await fetch("http://127.0.0.1:8000/predict", {
+    const res = await fetch("https://healthcare-backend-lg75.onrender.com", {
       method: "POST",
       body: formData,
     });
